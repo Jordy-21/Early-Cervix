427c3f2084d17483c77d2edccfeafff5a52aa6db
Jordy QC <94817291+Jordy-21@users.noreply.github.com>
2023-05-29 15:24:26 -0500

    Progress y Prolog

---
+++ progress.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', function() {
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    const scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
+    const windowHeight = document.documentElement.clientHeight || window.innerHeight;
+    
+    const scrolled = (scrollTop / (scrollHeight - windowHeight)) * 100;
+    
+    document.getElementById('progress-bar').style.width = scrolled + '%';
+  });
